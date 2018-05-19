57c7e3deb877d8259cc717e7029038822a2d6dda
hesiyuan <hesiyuan117@qq.com>
2018-05-19 14:48:46 +0800

    compose image from two cameras

--- video/OpenCamera.py
+++ video/OpenCamera.py
@@ -2,7 +2,7 @@
 
 import sys, os, time
 
-
+import PIL.Image
 import cv2
 
 
@@ -37,6 +37,40 @@ def OpenMultiCameras():
     cv2.destroyAllWindows()
 
 
+def DisplayCameraInOne():
+    cap = cv2.VideoCapture(0)
+    cap2 = cv2.VideoCapture(1)
+    if cap.isOpened():
+        # get vcap property
+        width = int (cap.get(cv2.CAP_PROP_FRAME_WIDTH  ) ) # float
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT ))  # float
+
+
+    while (1):
+        # get a frame
+        ret, frame = cap.read()
+        ret2, frame2 = cap2.read()
+        imgList = []
+        imgList.append(convertCvToPil(frame))
+        imgList.append(convertCvToPil(frame2))
+        import ImageCompose
+        tarImg = ImageCompose.imageJiontByRaw(imgList , width ,height,ImageCompose.OptImg.horizontal)
+
+        # show a frame
+        cv2.imshow("capture", tarImg)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cap2.release()
+    cv2.destroyAllWindows()
+
+
+
+def convertCvToPil(tImg):
+    cv2_im = cv2.cvtColor(tImg,cv2.COLOR_BGR2RGB)
+    pil_im = PIL.Image.fromarray(cv2_im)
+    return pil_im
+
 
 class MainTest():
     @staticmethod
@@ -47,7 +81,11 @@ class MainTest():
     def testOpenMultiCamera():
         OpenMultiCameras()
 
+    @staticmethod
+    def testMutilScreen():
+        DisplayCameraInOne()
+
 
 if __name__ == "__main__":
     print os.getcwd()
-    MainTest.testOpenMultiCamera()
+    MainTest.testMutilScreen()
