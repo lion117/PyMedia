0318a301058756b8052caec68ef87add6fdb38ad
hesiyuan <hesiyuan117@qq.com>
2018-06-26 20:14:32 +0800

    fetch the device name

--- Apply/AndroidOpt.py
+++ Apply/AndroidOpt.py
@@ -9,7 +9,7 @@ def tapScreen(x, y):
     # base_x, base_y = 1920, 1080
     # real_x = int(x / base_x * device_x)
     # real_y = int(y / base_y * device_y)
-    subprocess.call( 'D:\\program_test\\yeshen\\Nox\\bin\\nox_adb.exe shell  input tap %d %d '%(x,y))
+    subprocess.call( 'D:\\program_test\\yeshen\\Nox\\bin\\nox_adb.exe  shell   input tap %d %d '%(x,y))
 
 
 def clickScreen():
@@ -25,6 +25,20 @@ def screenShoot():
     subprocess.call(lCmd1)
     subprocess.call(lShoot)
 
+def fetchEmulatorDevice():
+    lExe = "D:\\program_test\\yeshen\\Nox\\bin\\nox_adb.exe"
+    lCmd = str.format('%s devices'%(lExe))
+    lRet = subprocess.Popen(lCmd,shell=True,stdout=subprocess.PIPE)
+    lInfo = lRet.stdout.read()
+    lDvList = parseDevices(lInfo)
+    return lDvList
+
+def parseDevices(tInfo):
+    if len(tInfo) == 0 :
+        return  []
+    import  re
+    lRet = re.findall(r"127.0.0.1:\d+",tInfo)
+    return  lRet
 
 class MainTest():
     @staticmethod
@@ -35,9 +49,16 @@ class MainTest():
     def testClick():
         tapScreen(733,451)
 
+    @staticmethod
+    def testAdbDevice():
+        lInfo = "'List of devices attached\r\n127.0.0.1:62001\tdevice\r\n"
+        parseDevices(lInfo)
+
 #com.tencent.tmgp.sgame
 
 if __name__ == "__main__":
     print os.getcwd()
     # MainTest.testScreenShoot()
-    MainTest.testClick()
+    # MainTest.testClick()
+    # fetchEmulatorDevice()
+    MainTest.testAdbDevice()
