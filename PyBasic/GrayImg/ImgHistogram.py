cb1a38690ad5d5e39259aedcf1d2ae19a9ce7ef8
hesiyuan <hesiyuan117@qq.com>
2018-07-03 12:24:58 +0800

    add py basic fucntion

--- PyBasic/GrayImg/ImgHistogram.py
+++ PyBasic/GrayImg/ImgHistogram.py
@@ -3,28 +3,10 @@
 import sys, os, time
 
 
-# import numpy as np
-# from skimage import exposure,data
-# image =data.camera()*1.0
-# hist1=np.histogram(image, bins=2)   #用numpy包计算直方图
-# hist2=exposure.histogram(image, nbins=2)  #用skimage计算直方图
-# print(hist1)
-# print(hist2)
-#
-#
-# from skimage import data
-# import matplotlib.pyplot as plt
-# img=data.camera()
-# plt.figure("hist")
-# arr=img.flatten()
-# n, bins, patches = plt.hist(arr, bins=256, normed=1,edgecolor='None',facecolor='red')
-# plt.show()
-
-
 import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
-
+import cv2
 
 def ConvertHist(tImage):
 
@@ -41,14 +23,36 @@ def ConvertHist(tImage):
     plt.show()
 
 
+def convertGrayByCv(tImage):
+    if os.path.exists(tImage) is False:
+        print(u"%s not found" % tImage)
+        return
+
+def cvRGBTest():
+    lParentDir = os.path.dirname(os.path.dirname(os.getcwd()))
+    lImageDir = os.path.join(lParentDir,u"ShareMedia/Images")
+    lFile = os.path.join(lImageDir,u"dog1.jpg")
+    lImage1 = Image.open(lFile)
+    lImage2 = cv2.imread(lFile)
+    lImage3 = Image.fromarray(cv2.cvtColor(lImage2, cv2.COLOR_BGR2RGB))
+    lImage4 =Image.fromarray(lImage2)
+
+    plt.subplot(221),plt.imshow(lImage3)
+    plt.subplot(222),plt.imshow(lImage4)
+    plt.subplot(223),plt.imshow(lImage1)
+    plt.subplot(224),plt.imshow(lImage2)
+    plt.show()
+
+
 
 
 class MainTest():
     @staticmethod
-    def testConvertHist():
+    def runConvertHist():
         lImage = u"hongkong.jpg"
         ConvertHist(lImage)
 
 if __name__ == "__main__":
     print os.getcwd()
-    MainTest.testConvertHist()
+    MainTest.runConvertHist()
+    # cvRGBTest()
