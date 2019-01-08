fae6463a65ef4d6127a94e5c1a4c578a939a03e1
leo <ngixpro@qq.com>
2019-01-08 22:14:27 +0800

    add FaceDective/FindFace.py

---
+++ app/FaceDective/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Leo
+Date:  2018/12/30
+Email:	lion_117@126.com
+All Rights Reserved Licensed under the Apache License
+"""
+
+import os, path
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+    pass
