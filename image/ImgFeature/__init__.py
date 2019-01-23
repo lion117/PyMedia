d81c4e38dd1f510fe834c0c941714a179e2c5767
hesiyuan <hesiyuan117@qq.com>
2019-01-23 10:51:15 +0800

    调整图像特征文件

---
+++ image/ImgFeature/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import sys, os, time
+
+
+class MainRun():
+
+    @classmethod
+    def runTest(cls):
+        print(u"hello world")
+
+
+if __name__ == "__main__":
+    MainRun.runTest()
