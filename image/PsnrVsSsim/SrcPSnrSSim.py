2b6a6b2d0e8b73d752262f8f15a5880acba10655
hesiyuan <hesiyuan117@qq.com>
2019-04-26 19:38:38 +0800

    add

--- image/PsnrVsSsim/SrcPSnrSSim.py
+++ image/PsnrVsSsim/SrcPSnrSSim.py
@@ -7,8 +7,8 @@ import  cv2
 class MainRun():
     _curDir = os.getcwd()
     _tarDir = os.path.dirname(os.path.dirname(_curDir))
-    _img1 = os.path.join(_tarDir, u"ShareMedia/Images/cat0_gray.jpg")
-    _img2 = os.path.join(_tarDir, u"ShareMedia/Images/cat0_gray.jpg")
+    _img1 = os.path.join(_tarDir, u"ShareMedia/Images/woman0.jpg")
+    _img2 = os.path.join(_tarDir, u"ShareMedia/Images/woman0_2.jpg")
 
     @classmethod
     def runTest(cls):
@@ -16,13 +16,13 @@ class MainRun():
 
     @classmethod
     def psnr(cls, tImg1, tImg2):
-        mse = (numpy.abs(tImg1 - tImg2) ** 2).mean()
+        mse = (numpy.abs(tImg1 - tImg2) ** 2).mean()+0.000001
         psnr = 10 * numpy.log10(255 * 255 / mse)
         return psnr
 
     @classmethod
     def ssim(cls, tImg1, tImg2):
-        assert len(tImg1.shape) == 2 and len(tImg2.shape) == 2
+        # assert len(tImg1.shape) == 2 and len(tImg2.shape) == 2
         assert tImg1.shape == tImg2.shape
         mu1 = tImg1.mean()
         mu2 = tImg2.mean()
@@ -44,12 +44,9 @@ class MainRun():
     def testPsnr(cls):
         lImg1 = cv2.imread(cls._img1)
         lImg2 = cv2.imread(cls._img2)
-        cv2.imshow("im1",lImg1)
-        cv2.imshow("im2",lImg2)
-
 
         lpsrn = MainRun.psnr(numpy.array(lImg1),numpy.array(lImg2))
-        print(lpsrn)
+        print("psrn:%f"%lpsrn)
 
 
 
@@ -58,11 +55,11 @@ class MainRun():
         lImg1 = cv2.imread(cls._img1)
         lImg2 = cv2.imread(cls._img2)
         lssim = MainRun.ssim(lImg1,lImg2)
-        print(lssim)
+        print("ssim:%f "%lssim)
 
 
 
 if __name__ == "__main__":
     MainRun.runTest()
     MainRun.testPsnr()
-    # MainRun.testSsim()
+    MainRun.testSsim()
